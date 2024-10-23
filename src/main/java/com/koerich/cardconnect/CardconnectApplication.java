0b42ec276a89ec273e514d4ea7326500a98f8412
Eduardo Lippold <kaia-palot@hotmail.com>
2024-10-23 11:32:34 -0300

    Initial commit

---
+++ src/main/java/com/koerich/cardconnect/CardconnectApplication.java
@@ -0,0 +1,13 @@
+package com.koerich.cardconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koerich/cardconnect/CardconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koerich.cardconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
